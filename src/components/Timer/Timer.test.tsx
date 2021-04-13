ed9110538a492b990a0e01121c34e65fe1b3fb23
San Martín Gabriel <gabisanmartin12@gmail.com>
2021-04-13 09:17:59 -0300

    Adds more tests and refactorizes other ones

--- src/components/Timer/Timer.test.tsx
+++ src/components/Timer/Timer.test.tsx
@@ -5,26 +5,37 @@ describe("<Timer />", () => {
   it("Component renders properly", () => {
     const { getByText } = render(<Timer />);
 
-    const time = getByText("25:00");
-    const button = getByText(/Start/i);
-
-    expect(time).toBeInTheDocument();
-    expect(button).toBeInstanceOf(HTMLButtonElement);
+    expect(getByText(/\d{2}:\d{2}/i)).toBeInTheDocument();
+    expect(getByText(/Start/i)).toBeInstanceOf(HTMLButtonElement);
   });
 
-  it("timer must starts when user press start button and button text must change to stop", async () => {
+  it("timer starts when user press start button and button text changes to stop", async () => {
     jest.useFakeTimers();
     const { getByText } = render(<Timer />);
 
-    fireEvent.click(getByText(/Start/i));
-
     act(() => {
-      jest.advanceTimersByTime(3000);
+      fireEvent.click(getByText(/Start/i));
+      jest.advanceTimersByTime(2000);
     });
 
     await waitFor(() => {
       expect(getByText("24:58")).toBeInTheDocument();
       expect(getByText(/stop/i)).toBeInTheDocument();
     });
+  });
+
+  it("timer stops when user presses the stop button and the button text changes to resume", async () => {
+    jest.useFakeTimers();
+    const { getByText } = render(<Timer />);
+
+    act(() => {
+      fireEvent.click(getByText(/Start/i));
+      jest.advanceTimersByTime(2000);
+      fireEvent.click(getByText(/Stop/i));
+      jest.advanceTimersByTime(2000);
+    });
+
+    expect(getByText("24:58")).toBeInTheDocument();
+    expect(getByText(/resume/i)).toBeInTheDocument();
   });
 });
